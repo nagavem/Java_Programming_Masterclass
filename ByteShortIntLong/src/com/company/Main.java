ba5db502aa5d418dc6e9b187a16c176489f9bbe3
Nagasudeep V <naga.sudeep@gmail.com>
2021-06-04 01:42:50 +0200

    Add files via upload

---
+++ ByteShortIntLong/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte var1 = 10;
+        short var2= 20;
+
+        int var3 = 50;
+
+        long result = 50000 +10 *(var1 + var2 +var3);
+        System.out.println(result);
+
+        short shortTotal =(short)( 1000 + 10 *(var1 + var2 +var3));
+
+
+
+    }
+}
